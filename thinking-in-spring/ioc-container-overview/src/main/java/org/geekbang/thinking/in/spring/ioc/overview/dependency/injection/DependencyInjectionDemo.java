8fb0e1a75c307f8b0e9beaa2a1ad4b5b905696b7
kzp <kangzhipeng94@163.com>
2023-05-13 17:53:18 +0800

    依赖来源

--- thinking-in-spring/ioc-container-overview/src/main/java/org/geekbang/thinking/in/spring/ioc/overview/dependency/injection/DependencyInjectionDemo.java
+++ thinking-in-spring/ioc-container-overview/src/main/java/org/geekbang/thinking/in/spring/ioc/overview/dependency/injection/DependencyInjectionDemo.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.env.Environment;
 
 /**
  * 依赖注入示例
@@ -16,10 +17,12 @@ public class DependencyInjectionDemo {
         //配置 XML配置文件
         //启动Spring 应用上下文
         BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:/META-INF/dependency-injection-context.xml");
+
+        //依赖来源一:自定义Bean
         UserRepository userRepository = beanFactory.getBean("userRepository",UserRepository.class);
         //System.out.println(userRepository.getUsers());
 
-        //依赖注入
+        //依赖来源二:依赖注入(内建依赖)
         System.out.println(userRepository.getBeanFactory());
 
 //        System.out.println(userRepository.getBeanFactory() == beanFactory); //false
@@ -32,5 +35,9 @@ public class DependencyInjectionDemo {
 
         //依赖查找（错误）
         //System.out.println(beanFactory.getBean(BeanFactory.class));
+
+        //依赖来源三:容器内建 Bean
+        Environment environment = beanFactory.getBean(Environment.class);
+        System.out.println("获取Environment 类型的Bean：" + environment);
     }
 }
