3ae03eb9ccf54a04eb8b7e928dc7369565489b66
kzp <kangzhipeng94@163.com>
2023-05-13 16:07:16 +0800

    Spring IoC依赖注入:Spring提供了哪些依赖注入模式和类型呢?

--- thinking-in-spring/ioc-container-overview/src/main/java/org/geekbang/thinking/in/spring/ioc/overview/dependency/injection/DependencyInjectionDemo.java
+++ thinking-in-spring/ioc-container-overview/src/main/java/org/geekbang/thinking/in/spring/ioc/overview/dependency/injection/DependencyInjectionDemo.java
@@ -1,6 +1,9 @@
 package org.geekbang.thinking.in.spring.ioc.overview.dependency.injection;
 
+import org.geekbang.thinking.in.spring.ioc.overview.repository.UserRepository;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -13,5 +16,21 @@ public class DependencyInjectionDemo {
         //配置 XML配置文件
         //启动Spring 应用上下文
         BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:/META-INF/dependency-injection-context.xml");
+        UserRepository userRepository = beanFactory.getBean("userRepository",UserRepository.class);
+        //System.out.println(userRepository.getUsers());
+
+        //依赖注入
+        System.out.println(userRepository.getBeanFactory());
+
+//        System.out.println(userRepository.getBeanFactory() == beanFactory); //false
+
+        //获取到SuperUser
+       /* ObjectFactory<User> objectFactory = userRepository.getUserObjectFactory();
+        System.out.println(objectFactory.getObject());*/
+        ObjectFactory<ApplicationContext> objectFactory = userRepository.getObjectFactory();
+        System.out.println(objectFactory.getObject() == beanFactory); //true
+
+        //依赖查找（错误）
+        //System.out.println(beanFactory.getBean(BeanFactory.class));
     }
 }
